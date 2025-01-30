0d08c5ea489a85c0f116a0b76821b7b2379e3c40
yaswanth <ykothakota198@gmail.com>
2025-01-31 01:24:10 +0530

    code

--- app.js
+++ app.js
@@ -2,30 +2,39 @@ var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+const cors = require('cors');
+
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'sdk/UI'));
+app.use(express.static(__dirname + '../public'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'sdk')));
+app.use(cors({
+  origin: '*', // Replace with your production domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/users/sts', users);
 
+app.options('*', cors());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
